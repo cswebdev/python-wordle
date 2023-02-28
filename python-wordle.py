e614a4e72db1eda9c146eae9d3cc333d492fed5c
Chelsea S <csnider91@live.com>
2023-02-28 16:57:44 -0500

    revised wordle so player can't guess a single letter

--- python-wordle.py
+++ python-wordle.py
@@ -10,7 +10,7 @@ newWords = []
 
 for word in words:
     if len(word) == 6:
-        newWords.append(word)
+        newWords.append(word.strip())
 
 words.close()
 
@@ -18,36 +18,42 @@ chosen_word = random.choice(newWords).lower()
 
 max_guesses = 5
 
-guessed_letters = []
-print("Lets play wordle!")
-print("Guess a letter or a five letter word")
+guessed_words = []
+print("Let's play wordle!")
+print("Guess a five letter word")
 display_word = '_' * len(chosen_word)
 print(display_word)
 
-for i in range(max_guesses):
-    # Get a letter guess from the user
-    guess = input('Guess a letter: ')
 
-    guessed_letters.append(guess)
+print(chosen_word)
+for i in range(max_guesses):
+    # Get a word guess from the user
+    guess = input('Guess a five letter word: ').lower()
+
+    if len(guess) != 5:
+        print('Please enter a five letter word')
+        continue
+
+    guessed_words.append(guess)
 
     display_word = ''.join(
-        [letter if letter in guessed_letters else '_' for letter in chosen_word])
+        [letter if letter in guessed_words[-1] else '_' for letter in chosen_word])
 
-    for letter in display_word:
-        if letter == guess:
-            print(colorama.Back.GREEN + letter +
-                  colorama.Style.RESET_ALL, end=' ')
+    for j, letter in enumerate(display_word):
+        if letter in guessed_words[-1]:
+            if j == guessed_words[-1].index(letter):
+                print(Back.GREEN + letter + Style.RESET_ALL, end=' ')
+            else:
+                print(Back.YELLOW + letter + Style.RESET_ALL, end=' ')
         else:
-            print(letter, end=' ')
+            print('_', end=' ')
     print()
 
-    if guess in chosen_word:
-        print('Correct!')
+    if guessed_words[-1] == chosen_word:
+        print('You win!')
+        break
     else:
         print('Incorrect.')
 
-    if display_word == chosen_word:
-        print('You win!')
-        break
-else:
+if guessed_words[-1] != chosen_word:
     print('You lose. The word was:', chosen_word)
